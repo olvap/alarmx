efaf29fed0f66660fc19420a3d85828ee537d759
Pablo Oldani <oldani.pablo@gmail.com>
2023-04-05 17:29:23 -0300

    remove ssl temporally

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,7 @@ Rails.application.configure do
   config.hosts << "alarmx.up.railway.app"
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.force_ssl = false
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
